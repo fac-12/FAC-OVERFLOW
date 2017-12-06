579d76f5095a9fe0cd39a8a9beb5921c2f58853a
dragomir <svedava@mail.ru>
2017-12-06 12:49:08 +0000

    build the database

---
+++ src/handler.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+const querystring = require('querystring');
+
+
+const homeHandler = (request, response) => {
+  const filePath = path.join(__dirname, '..', 'public', 'index.html')
+  fs.readFile(filePath, function(err, file) {
+    if (err) {
+      response.writeHead(500, {
+        'Content-Type': 'text/plain'
+      });
+      response.end('Server error');
+    }
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    response.end(file);
+  })
+}
+
+const staticFileHandler = (request, response, endpoint) => {
+  const extensionType = {
+    html: 'text/html',
+    css: 'txt/css',
+    js: 'application/javascript',
+    ico: 'image/x-icon',
+  }
+  const extension = endpoint.split('.')[1].split('?')[0];
+  const filePath = path.join(__dirname, '..', endpoint)
+  fs.readFile(filePath, function(err, file) {
+    if (err && err.code === 'ENOENT') {
+      response.writeHead(404, {
+        'Content-Type': 'text/plain'
+      });
+      response.end('404 Not Found');
+    } else if (err) {
+      response.writeHead(500, {
+        'Content-Type': 'text/plain'
+      });
+      response.end('Server error');
+    } else {
+      response.writeHead(200, 'Content-Type: ' + extensionType[extension]);
+      response.end(file);
+    }
+  })
+}
+
+
+
+module.exports = {
+  homeHandler,
+  staticFileHandler
+
+};
