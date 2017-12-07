f355e5566e345a5ef9b222a59df92b6bde327b74
Jemila <jemila.abulhawa@gmail.com>
2017-12-07 12:02:01 +0000

    change location of validateToken

--- src/handler.js
+++ src/handler.js
@@ -56,6 +56,30 @@ const staticFileHandler = (request, response, endpoint) => {
   })
 }
 
+const validateToken = (request, response) => {
+  const send401 = () => {
+    const message = 'You are not logged in';
+    response.writeHead(401, {
+      'Content-Type': 'text/plain'
+    });
+    return response.end(message);
+  }
+  const {
+    jwt
+  } = parse(request.headers.cookie);
+  if (!request.headers.cookie && !jwt) return send401();
+  return verify(jwt, process.env.SECRET, (jwt_err, jwt_res) => {
+    if (jwt_err) {
+      return send401()
+    } else {
+       const email = jwt_res;
+       console.log('I\'m email: ',typeof email)
+       response.writeHead(200, { 'Content-Type': 'text/plain'});
+       return response.end(email);
+    }
+  })
+}
+
 const signUpUser = (request, response) => {
   let allData = '';
   request.on('data', function (data) {
