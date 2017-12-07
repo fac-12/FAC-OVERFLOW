58cae1a75d39dfed8a6ee92017862d4fff1f0ec6
Shannon Jensen <shannonjensen@gmail.com>
2017-12-07 14:10:34 +0000

    fix client side validation

--- public/index.js
+++ public/index.js
@@ -12,50 +12,72 @@ var loginErrorMsg = document.getElementById('loginErrorMessage');
 
 // sort out passwords IMPORTANT
 
-
 login.addEventListener('submit', function (event) {
     event.preventDefault();
     if (loginEmail.value === "") {
-        loginErrorMsg.textContent = "Please enter an email."
-    }
-    if (loginErrorMsg.textContent === "") {
+        loginErrorMsg.textContent = "Please enter an email"
+    } else if (loginPassword.value === "") {
+        return loginErrorMsg.textContent = "Please enter your password"
+    } else if (checkEmail) {
         XHRRequest('/login', 'POST', JSON.stringify({email: loginEmail.value, password: loginPassword.value}), loginErrorMsg)
     }
 });
 
-signup.addEventListener('submit', function (event) {
-    event.preventDefault();
-    if (email.value === "") {
-        errorMsg.textContent = "Please enter an email."
-    } else {
-        errorMsg.textContent = badPassword(loginPassword.value);
-    }
-    if (errorMsg.textContent === "" && pw1.className === "") {
-        XHRRequest('/signup', 'POST', JSON.stringify({ email: email.value, password: pw1.value }), errorMsg)
+loginEmail.addEventListener('focusout', function () {
+    checkEmail(loginErrorMsg, loginEmail);
+});
+
+loginEmail.addEventListener('focus', function () {
+    loginEmail.className = '';
+});
+
+email.addEventListener('focusout', function () {
+    checkEmail(errorMsg, email)
+});
+
+email.addEventListener('focus', function () {
+    email.className = "";
+    if (pw1.className !== 'red'){
+        errorMsg.textContent = "";
     }
 });
 
 loginPassword.addEventListener('focus', function (){
-    loginErrorMsg.textContent = '';
-})
+    loginPassword.className = '';
+});
+
+var checkEmail = function (errMessage, emailBox) {
+    if (emailBox.value && (!validEmail(emailBox.value))) {
+        errMessage.textContent = "Invalid email address"
+        emailBox.className = "red";
+        return false;
+    } else {
+        emailBox.className = "";
+        errMessage.textContent = "";
+        return true;
+    }
+}
 
 var validEmail = function (email) {
     var regex = /^[\w-.]+@[\w-.]+.\w+$/;
     return regex.test(email);
 }
 
-var checkEmail = function (errMessage, emailBox) {
-    if (emailBox.value && (!validEmail(emailBox.value))) {
-        console.log(errMessage.textContent);
-        errMessage.textContent = "Invalid email address."
-        console.log(errMessage.textContent);
-        emailBox.className = "red";
+signup.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if (email.value === "") {
+        errorMsg.textContent = "Please enter an email"
+    } else if (!validEmail(email.value)) {
+        errorMsg.textContent = "Invalid email address"
+    } else if (!checkPasswords()) {
+        console.log("pw dont match");
+        errorMsg.textContent = "Passwords do not match"
+    } else if (badPassword(pw1.value)) {
+        errorMsg.textContent = badPassword(pw1.value);
     } else {
-        emailBox.className = "";
-        errMessage.textContent = "";
+        XHRRequest('/signup', 'POST', JSON.stringify({ email: email.value, password: pw1.value }), errorMsg)
     }
-}
-
+});
 
 var badPassword = function(password){
     var regex = /^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
@@ -63,53 +85,35 @@ var badPassword = function(password){
     if(regex.test(password)){
         return '';
     } else if (minEight.test(password)){
-        return 'Must include numbers and characters';
+        return 'Must include numbers & letters';
     } else {
         return 'Password not long enough (8+)';
     }
 }
-loginEmail.addEventListener('focusout', function () {
-    checkEmail(loginErrorMsg, loginEmail)
-});
-email.addEventListener('focusout', function () {
-    checkEmail(errorMsg, email)
-});
-loginEmail.addEventListener('focus', function () {
-    loginEmail.className = "";
-});
-email.addEventListener('focus', function () {
-    email.className = "";
-    if (pw1.className !== 'red'){
-        errMessage.textContent = "";
-    }
-});
 
 var checkPasswords = function (event) {
     if ((pw1.value !== pw2.value) && pw2.value && pw1.value) {
         errorMsg.textContent = "Passwords do not match";
         pw1.className = "red";
         pw2.className = "red";
+        console.log('passwords don\'t match')
+        return false;
     } else {
         pw1.className = "";
         pw2.className = "";
         errorMsg.className = "";
+        console.log('passwords match');
+        return true;
     }
 }
 
-pw1.addEventListener('focusout', function (event) {
-    if (pw1.value === "") {
-        errorMsg.textContent = "Please enter a password."
-    } else {
-        checkPasswords();
-    }
-});
+pw1.addEventListener('focusout', checkPasswords);
 
 pw2.addEventListener('focusout', checkPasswords);
 
 var resetPasswords = function (event) {
     pw1.className = "";
     pw2.className = "";
-    errorMsg.textContent = "";
 };
 
 pw1.addEventListener('focus', resetPasswords);
--- src/handler.js
+++ src/handler.js
@@ -65,15 +65,19 @@ const validateToken = (request, response) => {
     return response.end(message);
   }
   if (!request.headers.cookie) return send401();
-  const { jwt } = parse(request.headers.cookie);
-  if(!jwt) return send401();
+  const {
+    jwt
+  } = parse(request.headers.cookie);
+  if (!jwt) return send401();
   return verify(jwt, process.env.SECRET, (jwt_err, jwt_res) => {
     if (jwt_err) {
       return send401()
     } else {
-       const email = jwt_res;
-       response.writeHead(200, { 'Content-Type': 'text/plain'});
-       return response.end(email);
+      const email = jwt_res;
+      response.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      return response.end(email);
     }
   })
 }
@@ -141,38 +145,45 @@ const loginUser = (request, response) => {
     const password = userInfo.password;
     queries.emailInDatabase(email, (err, res) => {
       if (res === 0) {
-        response.writeHead(403, {"Content-Type" : "text/plain"})
+        response.writeHead(403, {
+          "Content-Type": "text/plain"
+        })
         return response.end('You need to sign up first!');
-      }
-      else if (res === 1) {
-       queries.getHash(email, (hash_err, hash_res) => {
-            if (hash_err) {
-              response.writeHead(500, {"Content-Type" : "text/plain"})
-             return response.end('server error password not found');
-            }
-            else {
-              bcrypt.compare(password, hash_res.rows[0].password, (bcrypt_err, bcrypt_res) => {
-                if (bcrypt_err) {
-                  response.writeHead(500, {"Content-Type" : "text/plain"})
-                 return response.end('server error');
-                }
-                else {
-                  if (bcrypt_res) {
-                    const cookie = sign(email, process.env.SECRET);
-                    response.writeHead(201, {"Location" : '/forum', 'Set-Cookie' : `jwt=${cookie}; HttpOnly`});
-                    return response.end();
-                  } else {
-                    response.writeHead(403, {"Content-Type" : 'text/plain'});
-                    return response.end('Password was incorrect');
-                  }
+      } else if (res === 1) {
+        queries.getHash(email, (hash_err, hash_res) => {
+          if (hash_err) {
+            response.writeHead(500, {
+              "Content-Type": "text/plain"
+            })
+            return response.end('server error password not found');
+          } else {
+            bcrypt.compare(password, hash_res.rows[0].password, (bcrypt_err, bcrypt_res) => {
+              if (bcrypt_err) {
+                response.writeHead(500, {
+                  "Content-Type": "text/plain"
+                })
+                return response.end('server error');
+              } else {
+                if (bcrypt_res) {
+                  const cookie = sign(email, process.env.SECRET);
+                  response.writeHead(201, {
+                    "Location": '/forum',
+                    'Set-Cookie': `jwt=${cookie}; HttpOnly`
+                  });
+                  return response.end();
+                } else {
+                  response.writeHead(403, {
+                    "Content-Type": 'text/plain'
+                  });
+                  return response.end('Password is incorrect');
                 }
               }
             });
           }
-        })
+        });
       }
-    })
-  })
+    });
+  });
 }
 
 module.exports = {
