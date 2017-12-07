cd8242f0c6ff519ae1ddd892a6299f9c5dff5b30
Jemila <jemila.abulhawa@gmail.com>
2017-12-07 12:54:22 +0000

    work on checkCookie function and getEmail

--- public/validate.js
+++ public/validate.js
@@ -1,11 +1,7 @@
-var userEmail = '';
-
 function checkCookie() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            userEmail = xhr.responseText;
-        } else if (xhr.readyState === 4 && xhr.status === 401){
+        if (xhr.readyState === 4 && xhr.status === 401){
             window.location.href = '/';
         }
     }
@@ -13,4 +9,5 @@ function checkCookie() {
     xhr.send();
 }
 
-checkCookie()
+checkCookie();
+
