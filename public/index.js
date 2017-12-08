fde76c43b9c6f582ffe63bc6ef22ae89ed93f306
Shannon Jensen <shannonjensen@gmail.com>
2017-12-08 09:38:37 +0000

    make front end code easy to read and fix some bugs"

--- public/index.js
+++ public/index.js
@@ -10,18 +10,91 @@ var pw2 = document.getElementById('confirmPassword');
 var errorMsg = document.getElementById('errorMessage');
 var loginErrorMsg = document.getElementById('loginErrorMessage');
 
-// sort out passwords IMPORTANT
+////Generic Functions for Validation
 
-login.addEventListener('submit', function (event) {
-    event.preventDefault();
-    if (loginEmail.value === "") {
-        loginErrorMsg.textContent = "Please enter an email"
-    } else if (loginPassword.value === "") {
-        return loginErrorMsg.textContent = "Please enter your password"
-    } else if (checkEmail) {
-        XHRRequest('/login', 'POST', JSON.stringify({email: loginEmail.value, password: loginPassword.value}), loginErrorMsg)
+//Sends and handles post requests
+var postRequest = function (url, body, errMsgBox) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 201) {
+            window.location.href = xhr.getResponseHeader('Location');
+        } else if (xhr.readyState === 4 && xhr.status >= 400) {
+            errMsgBox.textContent = xhr.responseText;
+        }
     }
-});
+    xhr.open('POST', url, true);
+    xhr.send(body);
+}
+
+//checks if email address valid, returns boolean and sets error messages accordingly
+var checkEmail = function (errMessage, emailBox) {
+    if (emailBox.value && (!validEmail(emailBox.value))) {
+        errMessage.textContent = "Invalid email address"
+        emailBox.className = "red";
+        return false;
+    } else {
+        emailBox.className = "";
+        if (errMessage.textContent.includes('email')) {
+            errMessage.textContent = "";
+        }
+        return true;
+    }
+}
+
+//tests email against regex, returns boolean
+var validEmail = function (email) {
+    var regex = /^[\w-.]+@[\w-.]+.\w+$/;
+    return regex.test(email);
+}
+
+//test password against regex, return error message or empty string
+var badPassword = function(password){
+    var regex = /^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
+    var minEight = /^.{8,}$/;
+    if(regex.test(password)){
+        return '';
+    } else if (minEight.test(password)){
+        return 'Password must include numbers & letters';
+    } else {
+        return 'Password must be at least 8 characters';
+    }
+}
+
+//reset password error
+var resetPasswordError = function (event) {
+    pw1.className = "";
+    pw2.className = "";
+    if (errorMsg.textContent.includes('Password')) {
+        errorMsg.textContent = "";
+    }
+};
+
+//check if passwords match and sets error messages accordingly
+var checkPasswords = function (event) {
+    var error = '';
+    if (pw1.value && pw2.value) {
+        error = badPassword(pw1.value);
+        if (!error && (pw1.value !== pw2.value)) {
+            error = "Passwords do not match";
+        }
+        if (error) {
+            if (!errorMsg.textContent.includes('email')) {
+                errorMsg.textContent = error;
+            }
+            pw1.className = "red";
+            pw2.className = "red";
+            return false;
+        } else {
+            resetPasswordError();
+            return true;
+        }
+    } else {
+        resetPasswordError();
+        return true;
+    }
+}
+
+////Handle Log In and Validation
 
 loginEmail.addEventListener('focusout', function () {
     checkEmail(loginErrorMsg, loginEmail);
@@ -31,113 +104,50 @@ loginEmail.addEventListener('focus', function () {
     loginEmail.className = '';
 });
 
+loginPassword.addEventListener('focus', function (){
+    loginPassword.className = '';
+});
+
+login.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if (loginEmail.value === "") {
+        loginErrorMsg.textContent = "Please enter an email";
+    } else if (loginPassword.value === "") {
+        loginErrorMsg.textContent = "Please enter your password"
+    } else if (checkEmail) {
+        var url = JSON.stringify({email: loginEmail.value, password: loginPassword.value});
+        postRequest('/login', url, loginErrorMsg);
+    }
+});
+
+////Handle Sign Up and Validation
+
 email.addEventListener('focusout', function () {
     checkEmail(errorMsg, email)
 });
 
 email.addEventListener('focus', function () {
     email.className = "";
-    if (pw1.className !== 'red'){
-        errorMsg.textContent = "";
-    }
 });
 
-loginPassword.addEventListener('focus', function (){
-    loginPassword.className = '';
-});
-
-var checkEmail = function (errMessage, emailBox) {
-    if (emailBox.value && (!validEmail(emailBox.value))) {
-        errMessage.textContent = "Invalid email address"
-        emailBox.className = "red";
-        return false;
-    } else {
-        emailBox.className = "";
-        errMessage.textContent = "";
-        return true;
-    }
-}
-
-var validEmail = function (email) {
-    var regex = /^[\w-.]+@[\w-.]+.\w+$/;
-    return regex.test(email);
-}
+pw1.addEventListener('focusout', checkPasswords);
+pw2.addEventListener('focusout', checkPasswords);
+pw1.addEventListener('focus', resetPasswordError);
+pw2.addEventListener('focus', resetPasswordError);
 
 signup.addEventListener('submit', function (event) {
     event.preventDefault();
     if (email.value === "") {
-        errorMsg.textContent = "Please enter an email"
+        errorMsg.textContent = "Please enter an email";
     } else if (!validEmail(email.value)) {
-        errorMsg.textContent = "Invalid email address"
+        errorMsg.textContent = "Invalid email address";
     } else if (!checkPasswords()) {
         console.log("pw dont match");
-        errorMsg.textContent = "Passwords do not match"
+        errorMsg.textContent = "Passwords do not match";
     } else if (badPassword(pw1.value)) {
         errorMsg.textContent = badPassword(pw1.value);
     } else {
-        XHRRequest('/signup', 'POST', JSON.stringify({ email: email.value, password: pw1.value }), errorMsg)
+        var url = JSON.stringify({ email: email.value, password: pw1.value });
+        postRequest('/signup', url, errorMsg);
     }
 });
-
-var badPassword = function(password){
-    var regex = /^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
-    var minEight = /^.{8,}$/;
-    if(regex.test(password)){
-        return '';
-    } else if (minEight.test(password)){
-        return 'Must include numbers & letters';
-    } else {
-        return 'Password not long enough (8+)';
-    }
-}
-
-var checkPasswords = function (event) {
-    if ((pw1.value !== pw2.value) && pw2.value && pw1.value) {
-        errorMsg.textContent = "Passwords do not match";
-        pw1.className = "red";
-        pw2.className = "red";
-        console.log('passwords don\'t match')
-        return false;
-    } else {
-        pw1.className = "";
-        pw2.className = "";
-        errorMsg.className = "";
-        console.log('passwords match');
-        return true;
-    }
-}
-
-pw1.addEventListener('focusout', checkPasswords);
-
-pw2.addEventListener('focusout', checkPasswords);
-
-var resetPasswords = function (event) {
-    pw1.className = "";
-    pw2.className = "";
-};
-
-pw1.addEventListener('focus', resetPasswords);
-pw2.addEventListener('focus', resetPasswords);
-
-var XHRRequest = function (url, method, body, errMsgBox) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 201) {
-            window.location.href = xhr.getResponseHeader('Location');
-        } else if (xhr.readyState === 4 && xhr.status >= 400) {
-            errMsgBox.textContent = xhr.responseText;
-        }
-    }
-    xhr.open(method, url, true);
-    xhr.send(body);
-}
-
-function parseResponse(response) {
-    try {
-        return JSON.parse(response);
-    } catch (e) {
-        return {
-            err: "Not JSON"
-        };
-    }
-}
